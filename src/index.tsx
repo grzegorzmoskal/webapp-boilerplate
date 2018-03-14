79124f52b82d026252a398ac1720b88cb5a50d6c
Grzegorz Moskal <grzegorz.moskal@tooploox.com>
2018-03-14 18:37:13 +0100

    tslint-config-prettier + tslint.config

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import * as React from "react"
 import { render } from "react-dom"
 import { Title } from "./components/Title"
-render(<Title text="Hello world!" />, document.getElementById("app"))
+render(
+    <div>
+        {process.env.APP_DESCRIPTION}
+        <Title text="Hello world!" />
+    </div>,
+    document.getElementById("app")
+)
